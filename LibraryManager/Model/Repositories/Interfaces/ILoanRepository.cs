3622f8f7adefb7d398e44c4748e057b6302b0223
ss19190 <sarass19190@gmail.com>
2025-03-20 10:31:50 +0100

    Task done

---
+++ LibraryManager/Model/Repositories/Interfaces/ILoanRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManager.Model.Entities;
+
+namespace LibraryManager.Model.Repositories.Interfaces
+{
+    internal interface ILoanRepository
+    {
+        IEnumerable<Loan> GetAll();
+        Loan GetById(int id);
+        void Insert(Loan book);
+        void Update(Loan book);
+        void Delete(Loan book);
+        void Save();
+
+        
+    }
+}
---
+++ LibraryManager/Model/Repositories/Interfaces/IUserRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LibraryManager.Model.Entities;
+
+namespace LibraryManager.Model.Repositories.Interfaces
+{
+    internal interface IBookRepository
+    {
+        IEnumerable<Book> GetAll();
+        Book GetById(int id);
+        void Insert(Book book);
+        void Update(Book book);
+        void Delete(Book book);
+        void Save();
+
+        
+    }
+}
