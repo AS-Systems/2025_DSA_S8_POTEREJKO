ac086d604ba0a11bdfe697fac4ae1ba5c16d5211
Alicja Banaszewska <alicja.banaszewska@op.pl>
2025-03-28 16:07:56 +0100

    Fixed main window. It's pretty now :3

---
+++ LibraryManager/View/Windows/MainWindow2.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace LibraryManager.View.Windows
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow2.xaml
+    /// </summary>
+    public partial class MainWindow2 : Window
+    {
+        public MainWindow2()
+        {
+            InitializeComponent();
+        }
+
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+    }
+}
