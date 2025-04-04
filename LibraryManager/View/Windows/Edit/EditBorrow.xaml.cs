d86ae240729aa3258bed3959f2e7003d20758aca
Alicja Banaszewska <alicja.banaszewska@op.pl>
2025-04-04 21:04:11 +0200

    Edit Windows Finished

---
+++ LibraryManager/View/Windows/Edit/EditBorrow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LibraryManager.Model.Entities;
+
+namespace LibraryManager.View.Windows.Edit
+{
+    /// <summary>
+    /// Logika interakcji dla klasy EditBorrow.xaml
+    /// </summary>
+    public partial class EditBorrow : Window
+    {
+        public EditBorrow(Borrow borrow)
+        {
+            InitializeComponent();
+            cmbBook.Text = borrow.Book.ToString();
+            cmbUser.Text = borrow.User.ToString();
+            //txtDescription.Text = borrow.Description;
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
---
+++ LibraryManager/View/Windows/Edit/EditUser.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using LibraryManager.Model.Entities;
+
+namespace LibraryManager.View.Windows.Edit
+{
+    /// <summary>
+    /// Logika interakcji dla klasy EditUser.xaml
+    /// </summary>
+    public partial class EditUser : Window
+    {
+        public EditUser(User user)
+        {
+            InitializeComponent();
+            txtName.Text = user.Name;
+            txtSurname.Text = user.Surname;
+            txtMail.Text = user.Email;
+            txtPhone.Text = user.PhoneNumber;
+        }
+        private void Border_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
