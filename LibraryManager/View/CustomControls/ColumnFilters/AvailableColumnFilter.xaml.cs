79f534fafeec21fa1f0b19dfeea07020d6936ad8
szymonpoterejkoinfe2 <sp306077@student.polsl.pl>
2025-04-14 22:22:55 +0200

    Feature: New custom controls

---
+++ LibraryManager/View/CustomControls/ColumnFilters/AvailableColumnFilter.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LibraryManager.View.CustomControls.ColumnFilters
+{
+    /// <summary>
+    /// Logika interakcji dla klasy AvailableColumnFilter.xaml
+    /// </summary>
+    public partial class AvailableColumnFilter : UserControl
+    {
+        public AvailableColumnFilter()
+        {
+            InitializeComponent();
+
+            Loaded += (s, e) =>
+            {
+                var window = Window.GetWindow(this);
+                if (window != null)
+                {
+                    window.LocationChanged += (sender, args) =>
+                    {
+                        if (PART_Popup != null)
+                        {
+                            double offset = PART_Popup.HorizontalOffset;
+                            PART_Popup.HorizontalOffset = offset + 1;
+                            PART_Popup.HorizontalOffset = offset;
+                        }
+                    };
+
+                    window.SizeChanged += (sender, args) =>
+                    {
+                        if (PART_Popup != null)
+                        {
+                            double offset = PART_Popup.HorizontalOffset;
+                            PART_Popup.HorizontalOffset = offset + 1;
+                            PART_Popup.HorizontalOffset = offset;
+                        }
+                    };
+                }
+            };
+        }
+
+        private void FilterButton_Click(object sender, RoutedEventArgs e)
+        {
+            PART_Popup.IsOpen = !PART_Popup.IsOpen;
+        }
+
+    }
+}
